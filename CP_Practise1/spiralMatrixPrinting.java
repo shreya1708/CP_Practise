391eaf0e6028dfe1e8ceb1f280b65a99a437f1df
shreya1708 <66875605+shreya1708@users.noreply.github.com>
2020-09-06 02:30:06 +0530

    Update spiralMatrixPrinting.java

--- CP_Practise1/spiralMatrixPrinting.java
+++ CP_Practise1/spiralMatrixPrinting.java
@@ -1,19 +1,31 @@
 import java.util.*;
-public class spiralMatrix {
-	public static void main(String[] args)
-	{
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int m[][]=  { {1,  2,  3,  4,  5},
-		        {6, 7,  8,  9,  10},
-		        {11, 12, 13, 14, 15},
-		        {16, 17, 18, 19, 20}
-		    };
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		int e=sc.nextInt();
+		int f=sc.nextInt();
+		int m[][]=  new int[e][f];
+		  for(int i=0;i<e;i++)
+		  {
+		      for(int j=0;j<f;j++)
+		      {
+		          m[i][j]=sc.nextInt();
+		      }
+		  }
 		int right=0;
 		int left=0;
 		int up=m[0].length;
 		int down =m.length;
 		int flag=0;
 		int flag1=1;
+		int count=0;
+		int x=up*down;
 		while(right<up&&left<down)
 		{
 			int i=0;
@@ -21,15 +33,23 @@ public class spiralMatrix {
 			 flag1=0;
 			for( i=right;i<up;i++)
 			{
+					if(count==x)
+				{
+				    break;
+					}
 				System.out.print(m[left][i]+" ");
+				count++;
 				flag1=1;
 			}
-			
 			int j=0;
 			for(j=left+1;j<down;j++)
 			{
-			     
+			        if(count==x)
+			        {
+			            break;
+			        }
 					System.out.print(m[j][up-1]+" ");
+					count++;
 					flag=1;
 			
 			}
@@ -40,13 +60,22 @@ public class spiralMatrix {
 				//System.out.println("hii"+right+" "+(down-1));
 				for( p=up-1-1;p>right;p--)
 				{
+				    if(count==x)
+				    {
+				        break;
+				    }
 						System.out.print(m[down-1][p]+" ");	
+						count++;
 				}
 				//i=p+1//
 				int q=0;
 				for( q=down-1;q>left;q--)
 				{
-			
+				    if(count==x)
+				    {
+				        break;
+				    }
+			           count++;
 						System.out.print(m[q][right]+" ");
 					
 				}
@@ -57,6 +86,8 @@ public class spiralMatrix {
 			left++;
 			right++;
 		}
+		System.out.println();
 	}
-
-}
+	}
+}		
+						 
